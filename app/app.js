1e638c1b72b9a06cc9e0bad494b0064c95392390
sricharan <sricharan100@yahoo.com>
2018-03-01 07:29:37 +0530

    second commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ var timeBomb = angular.module('timeBomb',['ngRoute']);
 timeBomb.config(['$routeProvider',function($routeProvider){
    $routeProvider.
    when('/bestseller',{
-     templateUrl:'views/bestseller.html'
+     templateUrl:'views/bestseller.html',
+     controller:'FoodController'
    }).
    when('/drinks',{
      templateUrl:'views/drinks.html'
@@ -23,184 +24,20 @@ timeBomb.config(['$routeProvider',function($routeProvider){
 
 }]);
 
-timeBomb.controller('FoodController',['$scope',function($scope){
-   $scope.foods = [
-  {
-    item:'POPCORN LARGE',
-    rate:140,
-    nos:0,
-    available:false
-  },
-  {
-    item:'CREAM DONUT',
-    rate:70,
-      nos:0,
-    available:false
-  },
-  {
-    item:'NACHOS WITH CHEESE',
-    rate:150,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CHICKEN PUFF',
-    rate:70,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CARAMEL POPCORN',
-    rate:120,
-      nos:0,
-    available:false
-  },
-  {
-    item:'COLD COFFEE',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CHILLI CHICKEN PIZZA',
-    rate:170,
-      nos:0,
-    available:false
-  },
 
-  {
-    item:'GARDEN VEG PIZZA',
-    rate:160,
-      nos:0,
-    available:false
-  },
-  {
-    item:'MARGHERITA PIZZA',
-    rate:160,
-      nos:0,
-    available:false
-  },
-  {
-    item:'PANNEER TIKKA PIZZA',
-    rate:160,
-      nos:0,
-    available:false
-  },
-  {
-    item:'VEG MASALA SANDWICH',
-    rate:140,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CHILLI CHICKEN SANDWICH',
-    rate:140,
-      nos:0,
-    available:false
-  },
-  {
-    item:'RED JUICE',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'WATERMELON JUICE',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'WATER',
-    rate:40,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CAPPUCCINO',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'GREEN TEA',
-    rate:110,
-      nos:0,
-    available:false
-  },
-  {
-    item:'COKE SMALL',
-    rate:90,
-      nos:0,
-    available:false
-  },
-  {
-    item:'HOT DOG',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CHICKEN TIKKA KAATI ROLL',
-    rate:110,
-      nos:0,
-    available:false
-  },
-  {
-    item:'VEG KAATI ROLL',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'PANNEER KAATI ROLL',
-    rate:115,
-      nos:0,
-    available:false
-  },
-  {
-    item:'CHICKEN KAATI ROLL',
-    rate:115,
-      nos:0,
-    available:false
-  },
-  {
-    item:'BHEL PURI',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'VADA PAV',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'SAMOSA',
-    rate:100,
-     nos:0,
-    available:false
-  },
-  {
-    item:'PAPDI CHAAT',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'JODHPURI CHAAT',
-    rate:100,
-      nos:0,
-    available:false
-  },
-  {
-    item:'DAHI BATATA PURI',
-    rate:100,
-      nos:0,
-    available:false
-  }
 
-];
 
-}])
+
+timeBomb.controller('FoodController',['$scope','$http',function($scope,$http){
+
+
+  $scope.additem = function (item) {
+
+        
+          console.log(item.available);
+}
+
+$http.get('data/bestseller.json').then(function(res){
+   $scope.foods = res.data;
+ })
+}]);
